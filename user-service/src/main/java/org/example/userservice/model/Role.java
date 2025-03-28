06024ebe77ad09e2dbe168f8cf2cbc14b9636147
Semyon Berezovskiy <ber2semen@gmail.com>
2025-03-28 22:47:11 +0700

    [Задание 1] миграции бд
    создал модельки, пытаюсь сгенерить chngelog файл с миграциями, все ошибки вроде убрал (nuild succesful) но файл не появился.

---
+++ user-service/src/main/java/org/example/userservice/model/Role.java
@@ -0,0 +1,19 @@
+package org.example.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ user-service/src/main/java/org/example/userservice/model/User.java
@@ -0,0 +1,36 @@
+package org.example.userservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles", // Таблица связки
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
